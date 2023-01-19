b6099895690f52ba2ab6f0d631a4e43cbd910887
Ikbal17 <mikbalf405@gmail.com>
2023-01-19 07:39:14 +0700

    ini project

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<?php
-$belakang = 'color:#64A7F5;';
-
-echo '    <html lang="en">
+<html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,13 +9,4 @@ echo '    <html lang="en">
       <h1>hallo wordl!!</h1>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
-</html>';
-?>
-  <link rel="stylesheet" href="path/style.css">
-  <style>
-
-    body{
-        background-color:<?php echo $belakang;?>
-    }
-
-  </style>
+</html>

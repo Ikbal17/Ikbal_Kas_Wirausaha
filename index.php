8d6fc9954cb6f74a49c2862af2c7c3485049d925
Ikbal17 <mikbalf405@gmail.com>
2023-01-20 09:39:57 +0700

    up

--- index.php
+++ index.php
@@ -1,28 +1,36 @@
+<?php
+  $username = 'admin';
+  $password = 'admin';
+
+  function login
+?>
+
+
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bootstrap demo</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="path/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">  <link rel="stylesheet" href="path/style.css">
 </head>
 <body>
   <br>
   <img class="logo" src="image/logo.png" alt=""><br><br><br><br>
   <br>
 <h1>Login</h1>
+ 
 <form action="POST">
   <div class="mb-3">
-    <input placeholder="Email/Username" type="email" class="input1" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input placeholder="Email/Username" type="email" class="input1" id="username" name="username" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
-    <input placeholder="Password" type="password" class="input2" id="exampleInputPassword1">
+    <input placeholder="Password" type="password" class="input2" id="password" name="password">
   </div>
-  <a href="" class="creat-akun">Buat Akun</a>
   <button type="submit" class="button">Login</button>
 </form>
 
 <!-- Ini JavaScirpt -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>

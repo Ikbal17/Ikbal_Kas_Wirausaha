529be577333b8aa1e8e19f204bb060dd63bb3456
Ikbal17 <mikbalf405@gmail.com>
2023-01-20 10:19:47 +0700

    up

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+include 'bot.php';
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,36 +1,36 @@
 <?php
+
+include "bot.php";
+?>
+<?php
+session_start();
   $username = 'admin';
   $password = 'admin';
+  if (isset($_POST['submit'])){
+    if($_POST ['username'] == $username && $_POST ['password'] == $password){
+      $_SESSION['username'] == $username;
+      header('home.php');
+    } else { 
+      login();
+      echo '<p>Kamu salah</p>';
+    }
+  }else{
+    login();
+  }
 
-  function login
-?>
-
-
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
-  <link rel="stylesheet" href="path/style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">  <link rel="stylesheet" href="path/style.css">
-</head>
-<body>
-  <br>
-  <img class="logo" src="image/logo.png" alt=""><br><br><br><br>
+  function login() { ?>
+   <img class="logo" src="image/logo.png" alt=""><br><br><br><br>
   <br>
 <h1>Login</h1>
+    <form action=<?php echo $_SERVER['PHP_SELF']?> method='POST'>
+    <div class="mb-3">
+      <input placeholder="Email/Username" type="text" class="input1" id="username" name="username" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <input placeholder="Password" type="password" class="input2" id="password" name="password">
+    </div>
+    <input type="submit" class="button" value="Login">
+  </form>
+  
+<?php }?>
  
-<form action="POST">
-  <div class="mb-3">
-    <input placeholder="Email/Username" type="email" class="input1" id="username" name="username" aria-describedby="emailHelp">
-  </div>
-  <div class="mb-3">
-    <input placeholder="Password" type="password" class="input2" id="password" name="password">
-  </div>
-  <button type="submit" class="button">Login</button>
-</form>
-
-<!-- Ini JavaScirpt -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html>

6960c5beb3407b9ebc4c8908d5bd5c607cc89a28
Ikbal17 <mikbalf405@gmail.com>
2023-01-20 11:29:00 +0700

    up

--- home.php
+++ home.php
@@ -1,17 +1,5 @@
 <?php
-include 'bot.php';
+
+include "bot.php";
 ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col">
-      Column
-    </div>
-    <div class="col">
-      Column
-    </div>
-    <div class="col">
-      Column
-    </div>
-  </div>
-</div>
--- index.php
+++ index.php
@@ -6,10 +6,10 @@ include "bot.php";
 session_start();
   $username = 'admin';
   $password = 'admin';
-  if (isset($_POST['submit'])){
-    if($_POST ['username'] == $username && $_POST ['password'] == $password){
-      $_SESSION['username'] == $username;
-      header('home.php');
+  if (isset($_POST['submit'])) {
+    if ($_POST['username'] == $username && $_POST['password'] == $password){
+      $_SESSION["username"] == $username;
+      echo "anda berhasil login $username";
     } else { 
       login();
       echo '<p>Kamu salah</p>';
@@ -18,18 +18,16 @@ session_start();
     login();
   }
 
-  function login() { ?>
+
+// ieu
+  function login(){ ?>
    <img class="logo" src="image/logo.png" alt=""><br><br><br><br>
   <br>
 <h1>Login</h1>
-    <form action=<?php echo $_SERVER['PHP_SELF']?> method='POST'>
-    <div class="mb-3">
-      <input placeholder="Email/Username" type="text" class="input1" id="username" name="username" aria-describedby="emailHelp">
-    </div>
-    <div class="mb-3">
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method='post'>
+      <input placeholder="Email/Username" type="text" class="input1" id="username" name="username">
       <input placeholder="Password" type="password" class="input2" id="password" name="password">
-    </div>
-    <input type="submit" class="button" value="Login">
+    <input type="submit" name="submit" class="button" value="Login">
   </form>
   
 <?php }?>

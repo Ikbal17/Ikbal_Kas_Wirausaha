6af2d94274c4028973304a3eb64bc81cd1bbfcb2
ikbal <mikbalf405@gmail.com>
2023-01-25 10:03:18 +0700

    up

--- index.php
+++ index.php
@@ -2,33 +2,26 @@
 
 include "bot.php";
 ?>
-<?php
-session_start();
-  $username = 'admin';
-  $password = 'admin';
-  if (isset($_POST["submit"])) {
-    if ($_POST['username'] == $username && $_POST['password'] == $password){
-      $_SESSION["username"] == $username;
-      echo "anda berhasil login $username";
-    } else { 
-      login();
-      echo '<p>Kamu salah</p>';
-    }
-  }else{
-    login();
-  }
-
-
-// ieu
-  function login(){ ?>
+<?php 
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan'] == "gagal"){
+			echo "Login gagal! username dan password salah!";
+		}else if($_GET['pesan'] == "logout"){
+			echo "Anda telah berhasil logout";
+		}else if($_GET['pesan'] == "belum_login"){
+			echo "Anda harus login untuk mengakses halaman admin";
+		}
+	}
+	?>
+  
    <img class="logo" src="image/logo.png" alt=""><br><br><br><br>
   <br>
 <h1>Login</h1>
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method='post'>
+    <form action="cek_login.php" method='post'>
       <input placeholder="Email/Username" type="text" class="input1" id="username" name="username">
       <input placeholder="Password" type="password" class="input2" id="password" name="password">
     <input type="submit" name="submit" class="button" value="Login">
   </form>
   
-<?php }?>
+
  
--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$koneksi = mysqli_connect("localhost", "root", "", "");
+$koneksi = mysqli_connect("localhost", "root", "", "db_ujikom");
 
 if(mysqli_connect_errno()){
     echo "koneksi database gagal :" . mysqli_connect_error();

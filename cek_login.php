fecb65839c47cbba9e753dfd7810b85a9482c6f8
Ikbal17 <mikbalf405@gmail.com>
2023-01-30 14:50:05 +0700

    up

--- cek_login.php
+++ cek_login.php
@@ -1,4 +1,23 @@
 <?php 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // mengaktifkan session php
 session_start();
  
@@ -10,16 +29,33 @@ $username = $_POST['username'];
 $password = $_POST['password'];
  
 // menyeleksi data admin dengan username dan password yang sesuai
-$data = mysqli_query($koneksi,"select * from tb_login where username='$username' and password='$password'");
- 
-// menghitung jumlah data yang ditemukan
-$cek = mysqli_num_rows($data);
- 
-if($cek > 0){
-	$_SESSION['username'] = $username;
+
+$sql = "SELECT username,password FROM tb_login";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $_SESSION['username'] = $username;
 	$_SESSION['status'] = "login";
 	header("location:admin/index.php");
-}else{
+  }
+} else {
 	header("location:index.php?pesan=gagal");
 }
+$conn->close();
+
+
+// $data = mysqli_query($koneksi,"select * from tb_login where username='$username' and password='$password'");
+ 
+// // menghitung jumlah data yang ditemukan
+// $cek = mysqli_num_rows($data);
+ 
+// if($cek > 0){
+// 	$_SESSION['username'] = $username;
+// 	$_SESSION['status'] = "login";
+// 	header("location:admin/index.php");
+// }else{
+// 	header("location:index.php?pesan=gagal");
+// }
 ?>
--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,31 @@
 <?php
+$servername = "localhost: 3306";
+$username = "root";
+$password = "";
+$database = "db_ujikom";
 
-$koneksi = mysqli_connect("localhost", "root", "", "db_ujikom");
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
 
-if(mysqli_connect_errno()){
-    echo "koneksi database gagal :" . mysqli_connect_error();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+// $koneksi = mysqli_connect("localhost", "root", "", "db_ujikom");
+
+// if(mysqli_connect_errno()){
+//     echo "koneksi database gagal :" . mysqli_connect_error();
+// }
 ?>
